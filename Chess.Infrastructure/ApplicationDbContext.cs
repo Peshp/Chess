69aa66ff1c6c2aacd9763388eaeb33f728f2af65
Anton Todorov <tonkisabe@abv.bg>
2025-07-14 14:28:45 +0300

    Refactoring

---
+++ Chess.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace Chess.Infrastructure
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
