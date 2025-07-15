c0bafb2cb2bab0aaa2bd9ec83182824da3a87bec
Anton Todorov <tonkisabe@abv.bg>
2025-07-15 17:11:06 +0300

    Create Figure.cs

---
+++ Chess.Domain/Entities/Figure.cs
@@ -0,0 +1,36 @@
+﻿namespace Chess.Domain.Entities
+{
+    using Enums;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Figure
+    {
+        public Figure()
+        {
+            MoveHistory = new HashSet<string>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public FigureType Type { get; set; }
+
+        [Required]
+        public int Row { get; set; }
+
+        [Required]
+        public int Col { get; set; }
+
+        [Required]
+        public string Color { get; set; } = string.Empty;
+
+        [Required]
+        public string FigureImage { get; set; } = string.Empty;
+
+        [Required]
+        public string CurrentPosition { get; set; } = string.Empty;
+
+        public ICollection<string> MoveHistory { get; set; } 
+    }
+}
