72e42c7c7dca9f4405d3e3d6e8a7554bb8f7879f
Anton Todorov <tonkisabe@abv.bg>
2025-07-15 17:11:15 +0300

    Update ApplicationDbContext.cs

--- Chess.Infrastructure/ApplicationDbContext.cs
+++ Chess.Infrastructure/ApplicationDbContext.cs
@@ -3,11 +3,24 @@
     using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore;
 
+    using Domain.Entities;
+
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Board> Boards { get; set; }
+
+        public DbSet<Figure> Figures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            
+
         }
     }
 }
