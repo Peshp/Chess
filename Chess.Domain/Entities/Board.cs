0bf73d3a4ab04f9692c6c94eb7f4cf08c8336f6e
Anton Todorov <tonkisabe@abv.bg>
2025-07-15 17:11:02 +0300

    Create Board.cs

---
+++ Chess.Domain/Entities/Board.cs
@@ -0,0 +1,16 @@
+﻿namespace Chess.Domain.Entities
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Board
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public string Image { get; set; } = string.Empty;
+    }
+}
