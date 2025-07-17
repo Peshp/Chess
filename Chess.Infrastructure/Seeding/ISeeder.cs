0ff485f21d31e36903d288796816701ec8b43567
Anton Todorov <tonkisabe@abv.bg>
2025-07-17 20:29:27 +0300

    Create ISeeder.cs

---
+++ Chess.Infrastructure/Seeding/ISeeder.cs
@@ -0,0 +1,13 @@
+﻿namespace Chess.Infrastructure.Seeding
+{
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    /// <summary>  
+    /// Seeds the database with initial data asynchronously.  
+    /// </summary>  
+    /// <param name="DbContext">The database context to seed.</param>  
+    public interface ISeeder<T> where T : class
+    {
+        void SeedDatabase();
+    }
+}
