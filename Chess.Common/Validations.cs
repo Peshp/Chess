9d6fdb6a820b12ef38dd0bf97d388d19e6da0d30
Anton Todorov <tonkisabe@abv.bg>
2025-07-19 15:16:18 +0300

    Create Validations.cs

---
+++ Chess.Common/Validations.cs
@@ -0,0 +1,18 @@
+﻿namespace Chess.Common
+{
+    public static class Validations
+    {
+        public static class Figure
+        {
+            public const int ColorMaxLength = 10;
+            public const int FigureImageMaxLength = 100;
+            public const int CurrentPositionMaxLength = 10;
+        }
+
+        public static class Board
+        {
+            public const int NameMaxLength = 50;
+            public const int ImageMaxLength = 500;
+        }
+    }
+}
