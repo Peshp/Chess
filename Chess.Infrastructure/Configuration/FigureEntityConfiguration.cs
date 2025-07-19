62958c4751d7c2199228d1007fd6d9795210c640
Anton Todorov <tonkisabe@abv.bg>
2025-07-19 15:16:30 +0300

    Create FigureEntityConfiguration.cs

---
+++ Chess.Infrastructure/Configuration/FigureEntityConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace Chess.Infrastructure.Configuration
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using Domain.Entities;
+
+    public class FigureEntityConfiguration : IEntityTypeConfiguration<Figure>
+    {
+        public void Configure(EntityTypeBuilder<Figure> builder)
+        {
+        }
+    }
+}
