0d92a9fbbf6cd6a71a2262f6540bc8d6c6c5c989
Anton Todorov <tonkisabe@abv.bg>
2025-07-24 23:40:01 +0300

    Update 20250721151931_SeedDatabaseFigureEntity.Designer.cs

--- Chess.Infrastructure/Migrations/20250721151931_SeedDatabaseFigureEntity.Designer.cs
+++ Chess.Infrastructure/Migrations/20250721151931_SeedDatabaseFigureEntity.Designer.cs
@@ -11,7 +11,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Chess.Infrastructure.Migrations
 {
-    [DbContext(typeof(ApplicationDbContext))]
+    [DbContext(typeof(ChessDbContext))]
     [Migration("20250721151931_SeedDatabaseFigureEntity")]
     partial class SeedDatabaseFigureEntity
     {
