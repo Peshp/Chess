ff6bedff8be2f3317f92a702d7a379dc3d597683
Anton Todorov <tonkisabe@abv.bg>
2025-07-24 23:39:56 +0300

    Create BoardEdntityConfiguration.cs

---
+++ Chess.Infrastructure/Configuration/BoardEdntityConfiguration.cs
@@ -0,0 +1,19 @@
+﻿namespace Chess.Infrastructure.Configuration
+{
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    using Domain.Entities;
+    using Infrastructure.Seeding;
+
+    public class BoardEdntityConfiguration : IEntityTypeConfiguration<Board>
+    {
+        private readonly BoardSeeder boardSeeder = new BoardSeeder();
+
+        public void Configure(EntityTypeBuilder<Board> builder)
+        {
+            builder
+                .HasData(boardSeeder.SeedDatabase());
+        }
+    }
+}
