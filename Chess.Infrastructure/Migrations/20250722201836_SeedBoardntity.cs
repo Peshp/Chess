6febe8f9eaf1f23613c1152943ba5578c73cb1c9
Anton Todorov <tonkisabe@abv.bg>
2025-07-25 22:24:11 +0300

    Create 20250722201836_SeedBoardntity.cs

---
+++ Chess.Infrastructure/Migrations/20250722201836_SeedBoardntity.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chess.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedBoardntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Boards",
+                columns: new[] { "Id", "Image", "Name" },
+                values: new object[] { 1, "board.png", "Standard Chess Board" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Boards",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
