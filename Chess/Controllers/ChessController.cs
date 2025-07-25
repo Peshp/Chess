1875f8562841a26d53cb87736dee5e64d8d8f26c
Anton Todorov <tonkisabe@abv.bg>
2025-07-25 22:24:23 +0300

    Create ChessController.cs

---
+++ Chess/Controllers/ChessController.cs
@@ -0,0 +1,16 @@
+﻿namespace Chess.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    using Chess.Application.Interfaces;
+
+    public class ChessController : Controller
+    {
+        private readonly IChessService chessService;
+
+        public IActionResult Game()
+        {
+            return View();
+        }
+    }
+}
