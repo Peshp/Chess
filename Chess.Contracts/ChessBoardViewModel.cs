52816ed9e7ee92ed90e0a157204a5c1e1805fc27
Anton Todorov <tonkisabe@abv.bg>
2025-07-30 16:32:15 +0300

    update

---
+++ Chess.Contracts/ChessBoardViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Chess.Contracts
+{
+    using Domain.Entities;
+
+    public class ChessBoardViewModel
+    {
+        public IList<Figure> BoardState { get; set; } = new List<Figure>();
+
+        public List<string> MoveHistory { get; set; } = new List<string>();
+
+        public string CurrentTurn { get; set; }
+
+        public string BoardImage { get; set; }
+
+        public string PiecesPath { get; set; }
+
+        public List<Figure> Figures { get; set; } = new List<Figure>();
+    }
+}
