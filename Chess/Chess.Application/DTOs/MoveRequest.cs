61d24532847dd5bc2c141295dc9a24bc667e52b9
Anton Todorov <tonkisabe@abv.bg>
2025-08-10 12:57:54 +0300

    Create MoveRequest.cs

---
+++ Chess/Chess.Application/DTOs/MoveRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace Chess.Application.DTOs
+{
+    public class MoveRequest
+    {
+        public int PieceId { get; set; }
+
+        public int ToX { get; set; }
+
+        public int ToY { get; set; }
+
+    }
+}
