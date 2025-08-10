387dc681e1a00855bafc758395321c6fcd5cf78c
Anton Todorov <tonkisabe@abv.bg>
2025-08-10 12:57:57 +0300

    Create ChessEngine.cs

---
+++ Chess/Chess.Application/ChessEngine.cs
@@ -0,0 +1,37 @@
+﻿namespace Chess.Application
+{
+    using infrastructure.Entities;
+
+    public class ChessEngine
+    {
+        private List<Figure> _figures;
+
+        public ChessEngine(List<Figure> figures)
+        {
+            _figures = figures;
+        }
+
+        public bool TryMove(int pieceId, int toX, int toY)
+        {
+            var piece = _figures.FirstOrDefault(f => f.Id == pieceId);
+            if (piece == null) return false;
+
+            if (IsValidMove(piece, toX, toY))
+            {
+                piece.PositionX = toX;
+                piece.PositionY = toY;
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool IsValidMove(Figure piece, int toX, int toY)
+        {
+            // TODO: Add real chess rules here
+            return true;
+        }
+
+        public List<Figure> GetUpdatedFigures() => _figures;
+    }
+}
