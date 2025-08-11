2448a86604031417e0cd6c92a0215095c6307d97
Anton Todorov <tonkisabe@abv.bg>
2025-08-11 13:43:33 +0300

    Create BoardStateCache.cs

---
+++ Chess/Chess.infrastructure/Extensions/BoardStateCache.cs
@@ -0,0 +1,9 @@
+﻿namespace Chess.infrastructure.Extensions
+{
+    using Chess.infrastructure.Entities;
+
+    public class BoardStateCache
+    {
+        public List<Figure> figures = new List<Figure>();
+    }
+}
