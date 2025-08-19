f8106e32e1e6df3d0554153a29da2a9b5d85099d
Anton Todorov <tonkisabe@abv.bg>
2025-08-19 21:38:40 +0300

    Update BoardViewModel.cs

--- Chess/Chess.Domain/ViewModels/Web/BoardViewModel.cs
+++ Chess/Chess.Domain/ViewModels/Web/BoardViewModel.cs
@@ -4,7 +4,9 @@ using System.Text.Json.Serialization;
 
 public class BoardViewModel
 {
-    public List<FigureViewModel> Figures { get; set; }
+    public List<FigureViewModel> Figures { get; set; } = new();
+
+    public List<FigureViewModel> CapturedFigures { get; set; } = new();
 
     public string BoardImage { get; set; }
 }
