4057e877712b8c14fe5ad39054a0eeec514c0d3a
Anton Todorov <tonkisabe@abv.bg>
2025-08-21 13:47:31 +0300

    Create ChessEngine.cs

---
+++ Chess/Chess.Application/ChessEngine/ChessEngine.cs
@@ -0,0 +1,57 @@
+﻿namespace Chess.Application.ChessEngine;
+
+using Application.ChessEngine.Validators;
+using Domain.ViewModels.Web;
+
+public class ChessEngine
+{
+    private readonly BoardViewModel _board;
+    private readonly Dictionary<string, IMoveValidator> _moveValidators;
+
+    public ChessEngine(BoardViewModel board)
+    {
+        _board = board;
+        _moveValidators = new Dictionary<string, IMoveValidator>
+        {
+            { "Pawn", new PawnMoveValidator() },
+            { "Bishop", new BishopMoveValidator() },
+            { "Rook", new RookMoveValidator() },
+            { "Queen", new QueenMoveValidator() },
+            { "King", new KingMoveValidator() },
+            { "Knight", new KnightMoveValidator() }
+            
+        };
+    }
+
+    public async Task<bool> TryMove(int pieceId, double toX, double toY)
+    {
+        var piece = _board.Figures.FirstOrDefault(f => f.Id == pieceId);
+        if (piece == null) return false;
+
+        if (await IsValidMove(piece, toX, toY))
+        {
+            var target = FindPiece(toX, toY);
+            if (target != null && target.Color != piece.Color)
+            {
+                _board.CapturedFigures.Add(target);
+                _board.Figures.Remove(target);
+            }
+
+            piece.PositionX = toX;
+            piece.PositionY = toY;
+            return true;
+        }
+        return false;
+    }
+
+    private async Task<bool> IsValidMove(FigureViewModel piece, double toX, double toY)
+    {
+        if (_moveValidators.TryGetValue(piece.Name, out var validator))
+            return validator.IsValidMove(piece, toX, toY, _board);
+
+        return false;
+    }
+
+    private FigureViewModel? FindPiece(double toX, double toY)
+        => _board.Figures.FirstOrDefault(f => f.PositionX == toX && f.PositionY == toY);
+}
