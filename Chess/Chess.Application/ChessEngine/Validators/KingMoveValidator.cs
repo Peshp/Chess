b4c884da4cbb823967f197dec8a97a9d4f131838
Anton Todorov <tonkisabe@abv.bg>
2025-08-21 13:47:41 +0300

    Create KingMoveValidator.cs

---
+++ Chess/Chess.Application/ChessEngine/Validators/KingMoveValidator.cs
@@ -0,0 +1,25 @@
+﻿namespace Chess.Application.ChessEngine.Validators;
+
+using Chess.Domain.ViewModels.Web;
+
+public class KingMoveValidator : IMoveValidator
+{
+    public bool IsValidMove(FigureViewModel piece, double toX, double toY, BoardViewModel board)
+    {
+        double dx = Math.Abs(piece.PositionX - toX);
+        double dy = Math.Abs(piece.PositionY - toY);
+
+        bool isOneStep = (dx <= 12.5) && (dy <= 12.5) && (dx + dy != 0);
+        bool isStraight = dx == 12.5 && dy == 0 || dx == 0 && dy == 12.5;
+        bool isDiagonal = dx == 12.5 && dy == 12.5;
+
+        if (!isOneStep || (!isStraight && !isDiagonal))
+            return false;
+
+        var target = board.Figures.FirstOrDefault(f => f.PositionX == toX && f.PositionY == toY);
+        if (target == null)
+            return true;
+
+        return target.Color != piece.Color; 
+    }
+}
