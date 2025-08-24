ced86af2f0b3ab8c283a899de9da5e339ced0f75
Anton Todorov <tonkisabe@abv.bg>
2025-08-24 15:00:27 +0300

    Update IGameService.cs

--- Chess/Chess.Application/interfaces/IGameService.cs
+++ Chess/Chess.Application/interfaces/IGameService.cs
@@ -7,4 +7,6 @@ public interface IGameService
     Task<BoardViewModel> GetBoard();
 
     Task<bool> TryMove(BoardViewModel board, int pieceId, double toX, double toY);
+
+    Task<bool> IsCheck(BoardViewModel board, string color);
 }
