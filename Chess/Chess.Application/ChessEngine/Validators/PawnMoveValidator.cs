96710612f7c0b5b7c6f8c061728c76fe27ec7716
Anton Todorov <tonkisabe@abv.bg>
2025-08-26 08:51:19 +0300

    Update PawnMoveValidator.cs

--- Chess/Chess.Application/ChessEngine/Validators/PawnMoveValidator.cs
+++ Chess/Chess.Application/ChessEngine/Validators/PawnMoveValidator.cs
@@ -6,8 +6,6 @@ public class PawnMoveValidator : IMoveValidator
 {
     public bool IsValidMove(FigureViewModel piece, double toX, double toY, BoardViewModel board)
     {
-        piece.IsMoved = true;
-
         double direction = piece.Color == "White" ? -12.5 : 12.5;
         double startRow = piece.Color == "White" ? 75 : 12.5;
 
@@ -16,27 +14,27 @@ public class PawnMoveValidator : IMoveValidator
             if (IsEmptySquare(toX, toY, board))
                 return true;
         }
-
         if (piece.PositionX == toX && piece.PositionY == startRow && piece.PositionY + direction * 2 == toY)
         {
             double intermediateY = piece.PositionY + direction;
             if (IsEmptySquare(toX, intermediateY, board) && IsEmptySquare(toX, toY, board))
                 return true;
         }
-
         if ((toX == piece.PositionX - 12.5 || toX == piece.PositionX + 12.5)
             && toY == piece.PositionY + direction)
         {
             var target = board.Figures.FirstOrDefault(f => f.PositionX == toX && f.PositionY == toY);
             if (target != null && target.Color != piece.Color)
                 return true;
-        }       
-
+        }
         return false;
     }
 
     private bool IsEmptySquare(double x, double y, BoardViewModel board)
     {
-        return board.Figures.All(f => f.PositionX != x || f.PositionY != y);
+        foreach (var f in board.Figures)
+            if (f.PositionX == x && f.PositionY == y)
+                return false;
+        return true;
     }
 }
