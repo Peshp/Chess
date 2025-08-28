6f2d09486f80409d7f4c88101c175df6c1516999
Anton Todorov <tonkisabe@abv.bg>
2025-08-28 15:56:23 +0300

    Delete

--- Chess.Common/GlobalConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Chess.Common
-{
-    public static class GlobalConstants
-    {
-        public const string SystemName = "Chess";
-
-        public const string AdministratorRoleName = "Administrator";
-    }
-}
