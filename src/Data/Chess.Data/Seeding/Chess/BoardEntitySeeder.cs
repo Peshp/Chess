6ebd87a693b7b24a7632217a1b623a920b27f975
Anton Todorov <tonkisabe@abv.bg>
2025-09-09 23:22:19 +0300

    Create BoardEntitySeeder.cs

---
+++ src/Data/Chess.Data/Seeding/Chess/BoardEntitySeeder.cs
@@ -0,0 +1,28 @@
+﻿namespace Chess.Data.Seeding.Chess
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using global::Chess.Data.Models;
+
+    public class BoardEntitySeeder : IEntitySeeder
+    {
+        public async Task Seed(ChessDbContext dbContext)
+        {
+            Board board = this.SeedBoard();
+
+            await dbContext.AddRangeAsync(board);
+        }
+
+        private Board SeedBoard()
+        {
+            var board = new Board
+            {
+                Id = 1,
+                Image = "board.png",
+            };
+
+            return board;
+        }
+    }
+}
