fbb8e3c2433eb1bb0ec53f06eeacb1f0fc43ecf3
Anton Todorov <tonkisabe@abv.bg>
2025-09-11 15:14:16 +0300

    Create Square.cs

---
+++ src/Data/Chess.Data.Models/Square.cs
@@ -0,0 +1,13 @@
+﻿namespace Chess.Data.Models
+{
+    public class Square
+    {
+        public int Id { get; set; }
+
+        public double PositionX { get; set; }
+
+        public double PositionY { get; set; }
+
+        public string Coordinate { get; set; }
+    }
+}
