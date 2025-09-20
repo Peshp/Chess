ab5658d82cc6a615b6abd1be5c7ce377422f9645
Anton Todorov <tonkisabe@abv.bg>
2025-09-20 14:22:41 +0300

    Update GameController.cs

--- src/Web/Chess.Web/Controllers/GameController.cs
+++ src/Web/Chess.Web/Controllers/GameController.cs
@@ -13,11 +13,15 @@
 
     public class GameController : BaseController
     {
-        private readonly IGameService _gameService;
+        private readonly IGameService gameService;
+        private readonly ICheckService checkService;
+        private readonly ICastleService castleService;
 
-        public GameController(IGameService gameService)
+        public GameController(IGameService gameService, ICheckService checkService, ICastleService castleService)
         {
-            _gameService = gameService ?? throw new ArgumentNullException(nameof(gameService));
+            this.gameService = gameService ?? throw new ArgumentNullException(nameof(gameService));
+            this.checkService = checkService ?? throw new ArgumentNullException(nameof(checkService));
+            this.castleService = castleService ?? throw new ArgumentNullException(nameof(castleService));
         }
 
         public async Task<IActionResult> Game()
@@ -25,7 +29,7 @@
             BoardViewModel board = HttpContext.Session.GetBoard();
             if (board == null)
             {
-                board = await _gameService.GetBoard();
+                board = await this.gameService.GetBoard();
                 HttpContext.Session.SetBoard(board);
             }
             return View(board);
@@ -34,23 +38,57 @@
         [HttpPost]
         public async Task<IActionResult> MakeMove([FromBody] Move request)
         {
-            var board = this.HttpContext.Session.GetBoard();
-            if (board == null)
-                return Json(new { success = false });
+            var board = HttpContext.Session.GetBoard();
 
-            bool success = await _gameService.TryMove(board, request.PieceId, request.ToX * 12.5, request.ToY * 12.5);
+            var piece = board.Figures.FirstOrDefault(f => f.Id == request.PieceId);
+
+            double toX = request.ToX * 12.5;
+            double toY = request.ToY * 12.5;
+
+            if (!await this.checkService.IsLegalMove(board, piece, toX, toY))
+            {
+                return Json(new { success = false });
+            }
+
+            bool result = false;
+
+            if (piece.Name == "King" && this.castleService.IsCastleAttempt(piece, toX, toY))
+            {
+                if (await this.castleService.IsCastleLegal(board, piece, toX, toY))
+                {
+                    this.castleService.PerformCastleMove(piece, board, toX, toY);
+                    board.CurrentTurn = (board.CurrentTurn == "White") ? "Black" : "White";
+                    result = true;
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Illegal castle move." });
+                }
+            }
+            else
+            {
+                if (board.CurrentTurn != piece.Color)
+                    return Json(new { success = false, message = "Not your turn." });
+
+                result = await this.gameService.TryMove(board, request.PieceId, toX, toY);
+            }
 
             bool isCheck = false;
-            if (success)
+            bool hasEscape = true;
+            if (result)
             {
                 HttpContext.Session.SetBoard(board);
-                isCheck = await _gameService.IsCheck(board, board.CurrentTurn);
+
+                string opponentColor = (board.CurrentTurn == "White") ? "Black" : "White";
+                isCheck = await this.checkService.IsCheck(board, opponentColor);
+                hasEscape = await this.checkService.HasLegalMoveToEscapeCheck(board, opponentColor);
             }
 
             return Json(new
             {
-                success,
+                success = result,
                 isCheck,
+                hasEscape,
                 currentTurn = board.CurrentTurn,
                 figures = board.Figures.Select(f => new
                 {
