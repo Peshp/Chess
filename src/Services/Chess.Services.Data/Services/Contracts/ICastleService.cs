1012bbca8e200a79d43ed6e3cace206b8a22f973
Anton Todorov <tonkisabe@abv.bg>
2025-09-20 14:22:56 +0300

    Create ICastleService.cs

---
+++ src/Services/Chess.Services.Data/Services/Contracts/ICastleService.cs
@@ -0,0 +1,15 @@
+﻿using Chess.Web.ViewModels.Chess;
+using System;
+using System.Threading.Tasks;
+
+namespace Chess.Services.Data.Services.Contracts
+{
+    public interface ICastleService
+    {
+        Task<bool> IsCastleLegal(BoardViewModel board, FigureViewModel king, double toX, double toY);
+
+        void PerformCastleMove(FigureViewModel king, BoardViewModel board, double toX, double toY);
+
+        bool IsCastleAttempt(FigureViewModel king, double toX, double toY);
+    }
+}
