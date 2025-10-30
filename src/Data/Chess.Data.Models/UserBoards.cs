d3de86bf496f69c3ea5da287f5ed948bf3293ff2
Anton Todorov <tonkisabe@abv.bg>
2025-10-30 17:57:55 +0200

    Create UserBoards.cs

---
+++ src/Data/Chess.Data.Models/UserBoards.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Chess.Data.Models
+{
+    public class UserBoards
+    {
+        public int Board { get; set; }
+
+        public ICollection<Board> Boards { get; set; }
+
+        public string UserId { get; set; }
+
+        public ApplicationUser User { get; set; }
+    }
+}
