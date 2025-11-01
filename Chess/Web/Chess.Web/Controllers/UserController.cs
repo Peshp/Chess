04b4499e6761c9c044a66705d05943150977d9a2
Anton Todorov <tonkisabe@abv.bg>
2025-11-01 12:51:45 +0200

    Create UserController.cs

---
+++ Chess/Web/Chess.Web/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿namespace Chess.Web.Controllers
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using Chess.Services.Data.Services.Contracts;
+    using Chess.Web.ViewModels.User;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public class UserController : BaseController
+    {
+        private readonly IUserService userService;
+
+        public UserController(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public async Task<IActionResult> Profile()
+        {
+            IEnumerable<UserBoardsViewModel> boards = await userService.GetHistory();
+
+            return View(boards);
+        }
+    }
+}
