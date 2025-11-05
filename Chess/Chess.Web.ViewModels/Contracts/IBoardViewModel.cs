45c425e3111c3bfb935688c383b17004eddf8d5c
Peshp <antontodorov88@gmail.com>
2025-11-05 15:31:33 +0200

    Create IBoardViewModel.cs

---
+++ Chess/Chess.Web.ViewModels/Contracts/IBoardViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Chess.Web.ViewModels.Contracts
+{
+
+    using ViewModels.Chess;
+
+
+    public interface IBoardViewModel
+    {
+        int Id { get; set; }
+
+        string UserId { get; set; }
+
+        string Image { get; set; }
+
+        IEnumerable<SquareViewModel> MoveHistory { get; set; }
+
+        IEnumerable<FigureViewModel> Figures { get; set; }
+    }
+}
