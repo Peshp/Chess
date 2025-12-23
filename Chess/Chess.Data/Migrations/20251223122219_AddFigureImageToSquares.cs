fb3d9c3b5bf873a5008d875c8ea1f5c2d8e8f0e5
Peshp <antontodorov88@gmail.com>
2025-12-23 21:16:15 +0200

    Create 20251223122219_AddFigureImageToSquares.cs

---
+++ Chess/Chess.Data/Migrations/20251223122219_AddFigureImageToSquares.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chess.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFigureImageToSquares : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FigureImage",
+                table: "BoardSquares",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FigureImage",
+                table: "BoardSquares");
+        }
+    }
+}
