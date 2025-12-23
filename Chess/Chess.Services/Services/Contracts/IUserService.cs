ac9b12ccfef8b2f1c23fee23614da28b7ef0eb00
Peshp <antontodorov88@gmail.com>
2025-12-23 21:16:34 +0200

    Update IUserService.cs

--- Chess/Chess.Services/Services/Contracts/IUserService.cs
+++ Chess/Chess.Services/Services/Contracts/IUserService.cs
@@ -3,10 +3,15 @@
     using System.Collections.Generic;
     using System.Threading.Tasks;
 
+    using Chess.Web.ViewModels.Chess;
     using Chess.Web.ViewModels.User;
 
     public interface IUserService
     {
         Task<IEnumerable<UserBoardsViewModel>> GetHistory(string userId);
+
+        Task<UserBoardsViewModel> GetBoardDetails(int boardId, string userId);
+
+        Task<UserBoardsViewModel> Next(UserBoardsViewModel board, string figureImg, double toX, double toY);
     }
 }
