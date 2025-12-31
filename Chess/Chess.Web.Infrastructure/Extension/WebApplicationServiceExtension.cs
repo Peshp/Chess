4687a4daa2709b70eff32e423bfcc96efeaf3225
Peshp <antontodorov88@gmail.com>
2025-12-31 16:37:28 +0200

    Update WebApplicationServiceExtension.cs

--- Chess/Chess.Web.Infrastructure/Extension/WebApplicationServiceExtension.cs
+++ Chess/Chess.Web.Infrastructure/Extension/WebApplicationServiceExtension.cs
@@ -31,4 +31,31 @@ public static class WebApplicationServiceExtension
             service.AddScoped(interfaceType, implementationType);
         }
     }
+
+    public static void AddApplicationValidator(this IServiceCollection service, Type serviceType)
+    {
+        Assembly? serviceAssembly = Assembly.GetAssembly(serviceType);
+        if (serviceAssembly == null)
+        {
+            throw new InvalidOperationException("Invalid validator service type provided!");
+        }
+
+        Type[] serviceTypes = serviceAssembly
+            .GetTypes()
+            .Where(t => !t.IsAbstract && !t.IsInterface && t.GetInterface("IMoveValidator") != null)
+            .ToArray();
+
+        foreach (Type implementationType in serviceTypes)
+        {
+            Type? interfaceType = implementationType
+                .GetInterface($"IMoveValidator");
+            if (interfaceType == null)
+            {
+                throw new InvalidOperationException(
+                    $"No interface is provided for the service with name {implementationType.Name}");
+            }
+
+            service.AddScoped(interfaceType, implementationType);
+        }
+    }
 }  
