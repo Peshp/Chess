cf8674f4d26078b0513b8b7da025e6f05ae48bb0
Peshp <todorovanton88@gmail.com>
2026-03-27 16:43:06 +0200

    Update UserBoardEntityConfiguration.cs

--- Chess/Chess.Data/Configuration/UserBoardEntityConfiguration.cs
+++ Chess/Chess.Data/Configuration/UserBoardEntityConfiguration.cs
@@ -1,28 +1,27 @@
-﻿namespace Chess.Data.Configuration
+﻿namespace Chess.Data.Configuration;
+
+using Chess.Data.Models;
+using static Chess.Common.BoardEntityConstants;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+public class UserBoardEntityConfiguration : IEntityTypeConfiguration<UserBoard>
 {
-    using Chess.Data.Models;
-    using static Chess.Common.BoardEntityConstants;
-
-    using Microsoft.EntityFrameworkCore;
-    using Microsoft.EntityFrameworkCore.Metadata.Builders;
-
-    public class UserBoardEntityConfiguration : IEntityTypeConfiguration<UserBoard>
+    public void Configure(EntityTypeBuilder<UserBoard> builder)
     {
-        public void Configure(EntityTypeBuilder<UserBoard> builder)
-        {
-            builder
-                .Property<string>(x => x.Image)
-                .HasMaxLength(BoardImageMaxLength);
+        builder
+            .Property<string>(x => x.Image)
+            .HasMaxLength(BoardImageMaxLength);
 
-            builder
-                .Property<string>(x => x.UserId)
-                .IsRequired(false);
+        builder
+            .Property<string>(x => x.UserId)
+            .IsRequired(false);
 
-            builder
-                .HasOne<ApplicationUser>(x => x.User)
-                .WithMany()
-                .HasForeignKey(x => x.UserId)
-                .IsRequired(false);
-        }
+        builder
+            .HasOne<ApplicationUser>(x => x.User)
+            .WithMany()
+            .HasForeignKey(x => x.UserId)
+            .IsRequired(false);
     }
 }
